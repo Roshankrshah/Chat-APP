45b04412bc9983c53cc754c14b80df33f0e2c5a2
Roshan <shahroshan0720@gmail.com>
2023-09-05 20:11:38 +0530

    structure setup

--- public/chat.js
+++ public/chat.js
@@ -1,1 +1,6 @@
 var socket = io.connect('http://localhost:4124');
+
+const message = document.getElementById('message');
+const handle = document.getElementById('handle');
+const btn = document.getElementById('send');
+const output = document.getElementById('output');
