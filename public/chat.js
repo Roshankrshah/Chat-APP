476c922394d5e3fc8a4cf11f5d88cecc534b23d7
Roshan <shahroshan0720@gmail.com>
2023-09-05 20:37:02 +0530

    broadcast msg

--- public/chat.js
+++ public/chat.js
@@ -4,6 +4,7 @@ const message = document.getElementById('message');
 const handle = document.getElementById('handle');
 const btn = document.getElementById('send');
 const output = document.getElementById('output');
+const feedback = document.getElementById('feedback');
 
 btn.addEventListener('click',()=>{
     socket.emit('chat',{
@@ -12,6 +13,15 @@ btn.addEventListener('click',()=>{
     });
 });
 
+message.addEventListener('keypress',()=>{
+    socket.emit('typing',handle.value);
+})
+
 socket.on('chat',(data)=>{
+    feedback.innerHTML = '';
     output.innerHTML += '<p><strong>' + data.handle + ':</strong>'+ data.message +'</p>';
+});
+
+socket.on('typing',(data)=>{
+    feedback.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>';
 })
