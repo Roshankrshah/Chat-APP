c6f318bd4d601dc3c6ab3a1d4e1222b907f9d2b5
Roshan <shahroshan0720@gmail.com>
2023-09-05 19:55:36 +0530

    socket io setup

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const socket = require('socket.io');
 const app = express();
 
 app.use(express.static('public'));
 
 const server = app.listen(4124,()=>{
     console.log(`Server listening at http://localhost:4124`);
+});
+
+const io = socket(server);
+
+io.on('connection',(socket)=>{
+    console.log('made by sockect connectin',socket.id);
 });
